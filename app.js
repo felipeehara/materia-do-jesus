7ec2d886b1a42eff8c9f4456ccedd66d52b04e8d
Felipe Ehara <felipeyoug1@gmail.com>
2024-11-01 16:44:10 -0300

    CSS FINALIZADO

--- app.js
+++ app.js
@@ -1,39 +1,11 @@
-// const express = require('express');
-// const bodyParser = require('body-parser');
-// const pokemonRoutes = require('./routes/pokemonRoutes');
-// const trainerRoutes = require('./routes/trainerRoutes');
-// const { getPokemons } = require('./models/pokemon'); // Importe a função para obter os Pokémons
-
-// const app = express();
-
-// app.set('view engine', 'ejs');
-// app.set('views', './views');
-
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(express.static('public'));
-
-// // Rota para a página inicial (index.ejs)
-// app.get('/', (req, res) => {
-//     const pokemons = getPokemons(); // Obtenha a lista de Pokémons
-//     res.render('index', { pokemons }); // Passe a lista de Pokémons para a view
-// });
-
-// // Rotas para Pokémons e Treinadores
-// app.use('/pokemons', pokemonRoutes);
-// app.use('/trainers', trainerRoutes);
-
-// const PORT = process.env.PORT || 3000;
-
-// app.listen(PORT, () => {
-//     console.log(`Servidor rodando em http://localhost:${PORT}`);
-// });
-
 const express = require('express');
 const bodyParser = require('body-parser');
 const pokemonRoutes = require('./routes/pokemonRoutes');
 const trainerRoutes = require('./routes/trainerRoutes');
 
 const app = express();
+
+app.use(express.static('public'));
 
 // Configuração do motor de template EJS
 app.set('view engine', 'ejs');
